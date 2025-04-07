d35635af5aa9bb81cdbaf4a9711e244418565a22
ellennamation <ellennamation@gmail.com>
2025-04-07 16:34:24 -0500

    started booms

---
+++ Assets/Scripts/TempBombExplosion.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempBombExplosion : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            this.gameObject.transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
+            StartCoroutine(Kaboom());
+        }
+    }
+
+    IEnumerator Kaboom()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(this.gameObject); 
+    }
+}
