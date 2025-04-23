b74c1d38edaa67c228f4c4c98e3bee7254dc8360
garret-snitchler <134814821+garret-snitchler@users.noreply.github.com>
2025-04-23 17:02:10 -0500

    WIP

--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -1,24 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHUD : MonoBehaviour
 {
-    public int health;
+    public int maxHealth;
+    private int health;
     public int powerUpTime;
     public TMPro.TextMeshPro healthText;
+    public string RespawnScene;
     // Start is called before the first frame update
     void Start()
     {
+        health = maxHealth;
         SetHealthText();
     }
 
     //// Update is called once per frame
-    //void Update()
-    //{
+    void Update()
+    {
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
 
-    //}
+    public void HealPlayer(int heal)
+    {
+        if (health + heal > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else
+        {
+            health += heal;
+        }
+        SetHealthText();
+    }
 
     public void DamagePlayer(int damage)
     {
@@ -28,11 +49,25 @@ public class PlayerHUD : MonoBehaviour
 
     void SetHealthText()
     {
-        print(healthText);
         if (healthText != null)
         {
-            print(health);
             healthText.text = "Health: " + health.ToString();
+        }
+    }
+
+    IEnumerator GameOver()
+    {
+        if (RespawnScene != "" && RespawnScene != null)
+        {
+            print("Respawn Scene: " + RespawnScene);
+            SceneManager.LoadScene(RespawnScene);
+        }
+        else
+        {
+            print("Respawn scene not set, reloading current scene");
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+            yield return new WaitForEndOfFrame();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
         }
     }
 }
